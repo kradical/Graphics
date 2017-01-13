36e4ad300f89284d6a58ce4e68ed19d33bdcdb65
Konrad Schultz <schultzk@teach.csc.uvic.ca>
2017-01-13 00:21:52 +0000

    materials almost working

--- raytracer/material.h
+++ raytracer/material.h
@@ -1,4 +1,8 @@
+#ifndef MATERIALH
+#define MATERIALH
+
 class material {
 public:
     virtual bool scatter(const ray& r_in, const hit_record& rec, vec3& attenuation, ray& scattered) const = 0;
 };
+#endif
--- raytracer/raytracer.cc
+++ raytracer/raytracer.cc
@@ -3,11 +3,12 @@
 #include "camera.h"
 #include "hitable_list.h"
 #include "sphere.h"
+#include "lambertian.h"
+#include "metal.h"
 #include "float.h"
 
-vec3 color(const ray&, hitable*);
+vec3 color(const ray&, hitable*, int);
 float hit_sphere(const vec3&, float, const ray&);
-vec3 random_in_unit_sphere();
 
 int main() {
     int nx = 200;
@@ -16,11 +17,13 @@ int main() {
     
     camera cam;
 
-    hitable* list[2];
-    list[0] = new sphere(vec3(0, 0, -1), 0.5);
-    list[1] = new sphere(vec3(0, -100.5, -1), 100);
+    hitable* list[4];
+    list[0] = new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3(0.8, 0.3, 0.3)));
+	list[1] = new sphere(vec3(0, -100.6, -1), 100, new lambertian(vec3(0.8, 0.8, 0.0)));
+	list[2] = new sphere(vec3(1, 0, -1), 0.5, new metal(vec3(0.8, 0.6, 0.2)));
+	list[3] = new sphere(vec3(-1, 0, -1), 0.5, new metal(vec3(0.8, 0.8, 0.8)));
 
-    hitable* world = new hitable_list(list, 2);
+    hitable* world = new hitable_list(list, 4);
 
     std::cout << "P3\n" << nx << " " << ny << "\n255\n";
     for (int j = ny - 1; j >= 0; j--) {
@@ -49,22 +52,19 @@ int main() {
     }
 }
 
-vec3 color(const ray& r, hitable* world) {
+vec3 color(const ray& r, hitable* world, int depth) {
     hit_record rec;
-    if (world->hit(r, 0.0, FLT_MAX, rec)) {
-		vec3 target = rec.p + rec.normal + random_in_unit_sphere();
-        return 0.5 * color(ray(rec.p, target - rec.p), world);
+    if (world->hit(r, 0.01, FLT_MAX, rec)) {
+		ray scattered;
+		vec3 attenuation;
+		if (depth < 50 && rec.mat_ptr->scatter(r, rec, attenuation, scattered)) {
+			return attenuation * color(scattered, world, depth + 1);
+		} else {
+			return vec3(0, 0, 0);
+		}
     } else {
         vec3 unit_direction = unit_vector(r.direction);
         float t = 0.5 * (unit_direction.y() + 1.0);
         return (1 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
     }
-}
-
-vec3 random_in_unit_sphere() {
-	vec3 p;
-	do {
-		p = 2.0 * vec3(drand48(), drand48(), drand48()) - vec3(1, 1, 1);
-	} while (dot(p, p) >= 1.0);
-	return p;
 }
