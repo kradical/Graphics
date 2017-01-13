385f7123065f62da306b9d359d177497d540ee97
Konrad <konrad_here@hotmail.com>
2017-01-12 22:06:01 -0800

    change to spaces

--- raytracer/raytracer.cc
+++ raytracer/raytracer.cc
@@ -13,35 +13,35 @@ float hit_sphere(const vec3&, float, const ray&);
 int main() {
     int nx = 200;
     int ny = 100;
-	int ns = 100;
+    int ns = 100;
     
     camera cam;
 
     hitable* list[4];
     list[0] = new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3(0.8, 0.3, 0.3)));
-	list[1] = new sphere(vec3(0, -100.6, -1), 100, new lambertian(vec3(0.8, 0.8, 0.0)));
-	list[2] = new sphere(vec3(1, 0, -1), 0.5, new metal(vec3(0.8, 0.6, 0.2)));
-	list[3] = new sphere(vec3(-1, 0, -1), 0.5, new metal(vec3(0.8, 0.8, 0.8)));
+    list[1] = new sphere(vec3(0, -100.6, -1), 100, new lambertian(vec3(0.8, 0.8, 0.0)));
+    list[2] = new sphere(vec3(1, 0, -1), 0.5, new metal(vec3(0.8, 0.6, 0.2)));
+    list[3] = new sphere(vec3(-1, 0, -1), 0.5, new metal(vec3(0.8, 0.8, 0.8)));
 
     hitable* world = new hitable_list(list, 4);
 
     std::cout << "P3\n" << nx << " " << ny << "\n255\n";
     for (int j = ny - 1; j >= 0; j--) {
         for (int i = 0; i < nx; i++) {
-			vec3 col(0, 0, 0);
-			for (int s = 0; s < ns; s++) {
-				float u = float(i + drand48()) / float(nx);
+            vec3 col(0, 0, 0);
+            for (int s = 0; s < ns; s++) {
+                float u = float(i + drand48()) / float(nx);
                 float v = float(j + drand48()) / float(ny);
             
                 ray r = cam.get_ray(u, v);
             
                 vec3 p = r.point_at_parameter(2.0);
                 col += color(r, world, 0);
-			}
+            }
 
-			col /= float(ns);
-			
-			col = vec3(sqrt(col.r()), sqrt(col.g()), sqrt(col.b()));
+            col /= float(ns);
+            
+            col = vec3(sqrt(col.r()), sqrt(col.g()), sqrt(col.b()));
 
             int ir = int(255.99 * col.r());
             int ig = int(255.99 * col.g());
@@ -55,13 +55,13 @@ int main() {
 vec3 color(const ray& r, hitable* world, int depth) {
     hit_record rec;
     if (world->hit(r, 0.01, FLT_MAX, rec)) {
-		ray scattered;
-		vec3 attenuation;
-		if (depth < 50 && rec.mat_ptr->scatter(r, rec, attenuation, scattered)) {
-			return attenuation * color(scattered, world, depth + 1);
-		} else {
-			return vec3(0, 0, 0);
-		}
+        ray scattered;
+        vec3 attenuation;
+        if (depth < 50 && rec.mat_ptr->scatter(r, rec, attenuation, scattered)) {
+            return attenuation * color(scattered, world, depth + 1);
+        } else {
+            return vec3(0, 0, 0);
+        }
     } else {
         vec3 unit_direction = unit_vector(r.direction);
         float t = 0.5 * (unit_direction.y() + 1.0);
