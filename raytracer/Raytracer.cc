4e5b7213c2201c7d70eafa86fd1064a585fba1e3
Konrad <konrad_here@hotmail.com>
2017-01-22 20:09:09 -0800

    scatter lighting and fixed cutoff issue

--- raytracer/Raytracer.cc
+++ raytracer/Raytracer.cc
@@ -15,18 +15,19 @@
 #include "light/PointLight.h"
 #include "float.h"
 
-Vec3 color(const Ray&, Hitable*, PointLight**, int);
+Vec3 color(const Ray&, HitableList*, PointLight**, int);
+Vec3 totalLitColor(HitableList*, PointLight**, hit_record*, Vec3*);
 float hit_sphere(const Vec3&, float, const Ray&);
-Hitable* scene();
+HitableList* scene();
 PointLight** scene_lighting();
 
 int main() {
     int nx = 800; // width in pixels
     int ny = 600; // height in pixels
-    int ns = 5; // number of samples per pixel
+    int ns = 10; // number of samples per pixel
     
-    Camera cam(Vec3(-2, 2, 1), Vec3(0, 0, -1), Vec3(0, 1, 0), 90, float(nx) / float(ny), 0.1);
-    Hitable* world = scene();
+    Camera cam(Vec3(0, 1, 1), Vec3(0, 0, -1), Vec3(0, 1, 0), 90, float(nx) / float(ny), 0.01);
+    HitableList* world = scene();
     PointLight** lights = scene_lighting();
 
     std::cout << "P3\n" << nx << " " << ny << "\n255\n";
@@ -55,14 +56,13 @@ int main() {
     }
 }
 
-Hitable* scene() {
-    int n = 3;
+HitableList* scene() {
+    int n = 4;
     Hitable** list = new Hitable*[n];
     list[0] = new Plane(Vec3(0, -1, 0), Vec3(0, 1, 0), new Lambertian(Vec3(1, 0, 0)));
-    //list[1] = new Plane(Vec3(-10, 0, 0), Vec3(1, 0, 0), new Lambertian(Vec3(0.5, 0.1, 0.1)));
-    // list[0] = new Rect(Vec3(-2.5, 1, -3.5), Vec3(2.5, -1, -3.5), Vec3(-2.5, 1, 1.5), new Lambertian(Vec3(0.8, 0.8, 0)));
     list[1] = new Sphere(Vec3(0, 0, -1), 0.5, new Lambertian(Vec3(0.01, 1, 0.01)));
     list[2] = new Sphere(Vec3(1, 0, -1), 0.5, new Lambertian(Vec3(0.01, 0.01, 1)));
+    list[3] = new Plane(Vec3(4, 0, 0), Vec3(-1, 0, 0), new Lambertian(Vec3(0, 1, 0)));
 
     return new HitableList(list, n);
 }
@@ -70,39 +70,58 @@ Hitable* scene() {
 #define NUM_LIGHTS 1
 PointLight** scene_lighting() {
     PointLight** list = new PointLight*[NUM_LIGHTS];
-    //list[0] = new PointLight(Vec3(1, 1, 0), Vec3(1), 1);
-    list[0] = new PointLight(Vec3(0, 1, -1), Vec3(1), 1);
+    list[0] = new PointLight(Vec3(-1, 1, 0), Vec3(1));
+    list[1] = new PointLight(Vec3(0, 0, 0), Vec3(0.01)); // small light at camera
 
     return list;
 }
 
-Vec3 color(const Ray& r, Hitable* world, PointLight** lights, int depth) {
+Vec3 color(const Ray& r, HitableList* world, PointLight** lights, int depth) {
     hit_record rec;
+    
     if (world->hit(r, 0.01, FLT_MAX, rec)) {
-        hit_record initialHit = rec; // TODO remove this once looping thru hitlist.
-        
-        Vec3 totalColor = Vec3(0);
-        int lightsHit = 0;
+        hit_record initialHit = rec;
 
         Vec3 surfaceColor = Vec3();
         Ray scattered = Ray();
-        rec.mat_ptr->scatter(r, rec, surfaceColor, scattered);
-        
-        for (int i = 0; i < NUM_LIGHTS; i++){
-            Ray lightDirection = Ray(initialHit.p + 1e-4 * initialHit.normal, unit_vector(lights[i]->point - initialHit.p));
+        if (depth < 2 && rec.mat_ptr->scatter(r, rec, surfaceColor, scattered)) {
+            Vec3 totalColor = (totalLitColor(world, lights, &initialHit, &surfaceColor) + 0.1 * color(scattered, world, lights, depth + 1)) / 1.1;
+            return totalColor;
+        } else {
+            return Vec3(0);
+        }
+    } else {
+        return Vec3(0);
+    }
+}
 
-            // TODO: fix this to take light location into consideration and be a quick exiting loop
-            if (!world->hit(lightDirection, 0.01, FLT_MAX, rec)) {
-                lightsHit += 1;
-                totalColor += (surfaceColor * lights[i]->color) * lights[i]->intensity * std::max(float(0), dot(initialHit.normal, lightDirection.direction));;
+Vec3 totalLitColor(HitableList* world, PointLight** lights, hit_record* initialHit, Vec3* surfaceColor) {
+    hit_record rec;
+
+    Vec3 totalColor = Vec3(0);
+    int lightsHit = 0;
+
+    for (int i = 0; i < NUM_LIGHTS; i++){
+        Vec3 toLight = lights[i]->point - initialHit->p;
+        Ray lightDirection = Ray(initialHit->p + 1e-4 * initialHit->normal, unit_vector(toLight));
+
+        bool hit_anything = true;
+        for (int j = 0; j < world->list_size; j++) {
+            if(world->list[j]->hit(lightDirection, 0.01, toLight.length(), rec)) {
+                hit_anything = false;
+                break;
             }
         }
-    
-        if (lightsHit > 1) {
-            totalColor /= lightsHit;
+
+        if (hit_anything) {
+            lightsHit += 1;
+            totalColor += (*surfaceColor * lights[i]->color) * std::max(float(0), dot(initialHit->normal, lightDirection.direction));;
         }
-        return totalColor;
-    } else {
-        return Vec3(0.1, 0.1, 0.5);
     }
+
+    if (lightsHit > 1) {
+        totalColor /= lightsHit;
+    }
+
+    return totalColor;
 }
--- raytracer/light/PointLight.h
+++ raytracer/light/PointLight.h
@@ -7,9 +7,8 @@ class PointLight {
 public:
 	Vec3 point;
 	Vec3 color;
-	float intensity;
 
-	PointLight(Vec3 p, Vec3 c, float i);
+	PointLight(Vec3 p, Vec3 c);
 };
 
 #endif
